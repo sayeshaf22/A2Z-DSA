6d6a5e259643c097a7bc24472c6ebc43068d9bb9
Syeda Ayesha Fatima <syedaayesha2211@gmail.com>
2022-10-22 19:09:34 +0530

    Pattern1 completed

---
+++ Basics/Patterns/Pattern1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Pattern1 {
+
+    static void printSquare(int n){
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printSquare(n);
+
+    }
+
+}
