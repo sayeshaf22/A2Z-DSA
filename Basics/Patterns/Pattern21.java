a859080a6c3e35da79bbb2a517d1d3732a83b59d
Syeda Ayesha Fatima <syedaayesha2211@gmail.com>
2022-10-26 19:44:04 +0530

    Pattern 20 - 21 Completed

---
+++ Basics/Patterns/Pattern21.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Pattern21{
+
+    static void printTriangle(int n){
+
+      for(int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                     System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printTriangle(n);
+
+    }
+
+
+
+}
